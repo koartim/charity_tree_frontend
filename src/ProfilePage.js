50a60305041f85755e5c3aea15c559a2fc345ce6
Tim Koar <koartimothy@gmail.com>
2019-08-12 14:35:18 -0400

    fixed profilepage

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -4,7 +4,7 @@ import { fetchCurrentUser } from './actions'
 import { fetchCharity } from './actions'
 import { fetchDonation } from './actions'
 import { Route, Switch } from 'react-router-dom'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Grid } from 'semantic-ui-react'
 import DonationForm from './DonationForm'
 
 class ProfilePage extends React.Component {
@@ -35,28 +35,27 @@ class ProfilePage extends React.Component {
 
   render() {
     if (this.state.donations.length > 0) {
-      console.log(this.getTotalAmount())
+      console.log(this.state.donations)
     }
     return(
     <Card>
       <Image align="center" src={this.props.selectedUser.user.avatar}/>
-      <Card.Content>
-        <Card.Header>username: {this.props.selectedUser.user.username}</Card.Header>
-        <Card.Meta>
-          <Card.Description>bio: {this.props.selectedUser.user.bio}</Card.Description>
-        </Card.Meta>
+        <Card.Header>{this.props.selectedUser.user.username}</Card.Header>
+        <Card.Header>
+          <Card.Description>{this.props.selectedUser.user.bio}</Card.Description>
+        </Card.Header>
         <Card.Meta>
         {this.state.donations.map(donation => {
           if (donation.user_id === this.props.selectedUser.user.id) {
-              return <Card.Description>Charity:{donation.charity.name},  Contribution: ${donation.amount}</Card.Description>
+              return <Card.Description>{donation.charity.name}: ${donation.amount}</Card.Description>
           }
         })}
         </Card.Meta>
         <Card.Meta>
-           <Card.Description> Total Contributions:{this.getTotalAmount()} </Card.Description>
+           <Card.Description> Total Donated:{this.getTotalAmount()} </Card.Description>
         </Card.Meta>
-      </Card.Content>
     </Card>
+
     )
   }
 }
